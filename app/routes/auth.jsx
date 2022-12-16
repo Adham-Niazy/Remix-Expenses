f9c2cfcdad84b2ffe4b1b9fb9c1e2c2cf45d8c6b
Adham-Niazy <adhamniazy@gmail.com>
2022-12-16 03:35:04 +0200

    Using Auth Form Component + updating the animation

--- app/routes/auth.jsx
+++ app/routes/auth.jsx
@@ -1,9 +1,8 @@
+import AuthForm from '~/components/auth/AuthForm';
 import AuthStyles from '~/styles/auth.css';
 
 export default function AuthPage() {
-  return (
-    <h1>Auth Page</h1>
-  );
+  return <AuthForm />;
 }
 
 export function links() {
