b144c0b452af6d44c011a5c2b38b34a75605438c
Adham-Niazy <adhamniazy@gmail.com>
2022-12-17 06:19:05 +0200

    Adding Server-side Validation

--- app/routes/__app/expenses/add.jsx
+++ app/routes/__app/expenses/add.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from "@remix-run/react";
 import ExpenseForm from "~/components/expenses/ExpenseForm";
 import Modal from "~/components/util/Modal";
 import { addExpense } from "~/data/expenses.server";
+import { validateExpenseInput } from "~/data/validation.server";
 
 export default function AddExpensesPage() {
   const navigate = useNavigate();
@@ -21,7 +22,12 @@ export default function AddExpensesPage() {
 export async function action({ request, params }) {
   const formData = await request.formData();
   const expenseData = Object.fromEntries(formData);
-  console.log(expenseData, formData);
+  
+  try {
+    validateExpenseInput(expenseData);
+  } catch (e) {
+    return e;
+  }
 
   await addExpense(expenseData);
   return redirect('/expenses')
