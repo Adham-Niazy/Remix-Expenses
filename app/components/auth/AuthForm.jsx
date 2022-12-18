6b84becb125daafa94be0be5e726d78f5e942b05
Adham-Niazy <adhamniazy@gmail.com>
2022-12-18 03:31:08 +0200

    Extracting searchParams

--- app/components/auth/AuthForm.jsx
+++ app/components/auth/AuthForm.jsx
@@ -1,4 +1,4 @@
-import { Link, useSearchParams } from '@remix-run/react';
+import { Form, Link, useSearchParams } from '@remix-run/react';
 import { FaLock, FaUserPlus } from 'react-icons/fa';
 
 function AuthForm() {
@@ -9,7 +9,7 @@ function AuthForm() {
   const toggleBtnCaption = authMode === 'login' ? 'Create a new user' : 'Log in with existing user';
 
   return (
-    <form method="post" className="form" id="auth-form">
+    <Form method="post" className="form" id="auth-form">
       <div className="icon-img">
         {authMode === 'login' ? <FaLock /> : <FaUserPlus />}
       </div>
@@ -25,7 +25,7 @@ function AuthForm() {
         <button>{submitBtnCaption}</button>
         <Link to={authMode === 'login' ? '?mode=signup' : '?mode=login'}>{toggleBtnCaption}</Link>
       </div>
-    </form>
+    </Form>
   );
 }
 
--- app/routes/__marketing/auth.jsx
+++ app/routes/__marketing/auth.jsx
@@ -5,6 +5,20 @@ export default function AuthPage() {
   return <AuthForm />;
 }
 
+export async function action({ request }) {
+  const searchParams = new URL(request.url).searchParams;
+  const authMode = searchParams.get('mode') || 'login';
+
+  const formData = await request.formData();
+  const userData = Object.fromEntries(formData);
+
+  if ( authMode === 'login' ) {
+    // Login Logic
+  } else {
+    // Signup Logic
+  }
+}
+
 export function links() {
   return [{ rel: 'stylesheet', href: AuthStyles }];
 }
