6e402e1f3f541cf2dc7b34cae0581930b33f96c4
Adham-Niazy <adhamniazy@gmail.com>
2022-12-18 06:05:15 +0200

    Adding Login Server-side function

--- app/data/auth.server.js
+++ app/data/auth.server.js
@@ -1,17 +1,32 @@
 import { prisma } from "./database.server";
-import { hash } from 'bcryptjs';
+import { hash, compare } from 'bcryptjs';
+
+function throwError(message, status) {
+  const error = new Error(message);
+  error.status = status;
+  throw error;
+}
 
 export async function signup({ email, password }) {
-  const existingUser = await prisma.user.findFirst( { where: {email} });
-  console.log(existingUser);
-
-  if ( existingUser ) {
-    const error = new Error('A user with the provided email address exists already!');
-    error.status = 422;
-    throw error;
+  // Checking Uniqueness of email
+  const existingUser = await prisma.user.findFirst({ where: { email } });
+  if (existingUser) {
+    throwError('A user with the provided email address exists already!', 422);
   }
 
   const passwordHash = await hash(password, 12);
-
   await prisma.user.create({ data: { email, password: passwordHash } })
+}
+
+export async function login({ email, password }) {
+  // Checking Email
+  const existingUser = await prisma.user.findFirst({ where: { email } });
+  if (!existingUser) {
+    throwError('Could not log you in, please check the provided credentials.', 401);
+  }
+  // Checking Password
+  const passwordCorrect = await compare(password, existingUser.password);
+  if (!passwordCorrect) {
+    throwError('Could not log you in, please check the provided credentials.', 401);
+  }
 }
