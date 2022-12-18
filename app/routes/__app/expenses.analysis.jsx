95234696c3184caa8895e2ba1f5a145f656fdf00
Adham-Niazy <adhamniazy@gmail.com>
2022-12-18 04:42:28 +0200

    Analysis Charts logic + Error Boundarys

--- app/routes/__app/expenses.analysis.jsx
+++ app/routes/__app/expenses.analysis.jsx
@@ -1,24 +1,44 @@
-import Chart from "~/components/expenses/Chart";
-import ExpenseStatistics from "~/components/expenses/ExpenseStatistics";
+import { useCatch, useLoaderData } from "@remix-run/react";
+import { json } from '@remix-run/node';
 
-const EXPENSES_DUMMY = [
-  {
-    id: 'e1',
-    title: 'First Expense',
-    amount: 12.99,
-    date: new Date().toISOString()
-  },
-  {
-    id: 'e2',
-    title: 'Second Expense',
-    amount: 1.99,
-    date: new Date().toISOString()
-  }
-]
+import Chart from "~/components/expenses/Chart";
+import Error from '~/components/util/Error';
+import ExpenseStatistics from "~/components/expenses/ExpenseStatistics";
+import { getExpenses } from '~/data/expenses.server';
 
 export default function AnalysisExpensesPage() {
+  const expenses = useLoaderData();
+
   return <main>
-    <Chart expenses={EXPENSES_DUMMY} />
-    <ExpenseStatistics expenses={EXPENSES_DUMMY} />
+    <Chart expenses={expenses} />
+    <ExpenseStatistics expenses={expenses} />
+  </main>
+}
+
+export async function loader() {
+  const expenses = await getExpenses();
+
+  if (!expenses || expenses.length === 0) {
+    throw json(
+      {
+        message: 'Could not load analysis for your empty list of expenses'
+      },
+      {
+        status: 404,
+        statusText: 'Expenses not found'
+      }
+    )
+  }
+
+  return expenses;
+}
+
+export function CatchBoundary() {
+  const caughtResponse = useCatch();
+
+  return <main>
+    <Error title={caughtResponse.statusText}>
+      <p>{caughtResponse.data?.message || 'Something went wrong - could not create your analysis'}</p>
+    </Error>
   </main>
 }
