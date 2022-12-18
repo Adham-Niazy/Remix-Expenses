b3c45b215b2f4475a2077b784c8a8553042b2fa1
Adham-Niazy <adhamniazy@gmail.com>
2022-12-18 04:09:46 +0200

    handle no expenses case

--- app/routes/__app/expenses.jsx
+++ app/routes/__app/expenses.jsx
@@ -7,6 +7,8 @@ import { getExpenses } from "~/data/expenses.server";
 export default function ExpensesLayout() {
   const expenses = useLoaderData();
 
+  const hasExpenses = expenses && expenses.length > 0;
+
   return <>
     <Outlet />
     <main>
@@ -20,7 +22,19 @@ export default function ExpensesLayout() {
           <span>Load Raw Data</span>
         </a>
       </section>
-      <ExpensesList expenses={expenses}/>
+      {
+        hasExpenses ? (
+          <ExpensesList expenses={expenses} />
+        ) : (
+          <section id="no-expenses">
+            <h1>No expenses found</h1>
+            <p>
+              Start <Link to="add">adding some</Link> today!
+            </p>
+          </section>
+        )
+      }
+
     </main>
   </>
 }
